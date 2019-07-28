48f7c7e5075d3105199d6358917f1284e665f3f9
Tim Heck <tih99924@bethel.edu>
2019-07-28 09:49:55 -0500

    modified details reducer, genres rendering on details page

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -12,12 +12,30 @@ router.get('/', (req, res) => {
     })
 })
 
+// router.get('/details/:id', (req, res) => {
+//     const id = req.params.id;
+//     const sqlText = 'SELECT * FROM movies WHERE id=$1;';
+//     pool.query(sqlText, [id]).then(result => {
+//         console.log(result.rows[0]);
+//         res.send(result.rows[0]);
+//     }).catch(err => {
+//         console.log('Error when getting specific movie by id', err);
+//         res.sendStatus(500);
+//     })
+// })
+
 router.get('/details/:id', (req, res) => {
     const id = req.params.id;
-    const sqlText = 'SELECT * FROM movies WHERE id=$1;';
+    const sqlText = `
+        SELECT movies.id, movies.title, movies.description, genres.name AS genre
+        FROM categorize_movies
+        JOIN movies ON movies.id = categorize_movies.movie_id
+        JOIN genres ON genres.id = categorize_movies.genre_id
+        WHERE categorize_movies.movie_id = $1;
+    `;
     pool.query(sqlText, [id]).then(result => {
-        console.log(result.rows[0]);
-        res.send(result.rows[0]);
+        console.log(result.rows);
+        res.send(result.rows);
     }).catch(err => {
         console.log('Error when getting specific movie by id', err);
         res.sendStatus(500);
